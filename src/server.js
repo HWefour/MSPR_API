aafce5e6ba753c99429d40338dd10b64c157f454
Givros <69457849+lucaslimouzin@users.noreply.github.com>
2024-01-29 11:44:29 +0100

    ajout cors

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const HomeRoute = require("./Routes/HomeRoute")
+const cors = require("cors"); // Import CORS package
+
+const HomeRoute = require("./Routes/HomeRoute");
 const ProfileRoute = require("./Routes/ProfileRoutes");
 const SettingsRoute = require("./Routes/SettingsRoute");
 const BackofficeRoute = require("./Routes/BackofficeRoutes");
 
-
-
+// Use CORS middleware (You can customize it as needed)
+app.use(cors()); // This will allow all CORS requests. You can also specify options to restrict domains, methods etc.
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/home" , HomeRoute);
+app.use("/home", HomeRoute);
 app.use("/profile", ProfileRoute);
-app.use("/settings" , SettingsRoute);
-app.use("/backoffice" , BackofficeRoute);
-
-
+app.use("/settings", SettingsRoute);
+app.use("/backoffice", BackofficeRoute);
 
 // TESTING THE SERVER
 app.get("/test", async (req, res) => {
