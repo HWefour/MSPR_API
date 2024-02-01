6ef6a5e30f232757da65569d0a4ed207c547a5e4
wefour <we46584@gmail.com>
2024-02-01 11:21:12 +0100

    module de connextion plus changement bdd

--- src/Models/Auth.js
+++ src/Models/Auth.js
@@ -3,10 +3,19 @@
 const knex = require("../Config/Knex");
 const bcrypt = require('bcrypt');
 
-const signUp = async (user) => {
-  const hashedPassword = await bcrypt.hash(user.password, 10);
-  return await knex('users').returning('*').insert({ ...user, password: hashedPassword });
-};
+async function signUp(user) {
+    // Assurez-vous que user contient également idRole
+    return knex("users").insert({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        usersName: user.usersName,
+        email: user.email,
+        city: user.city,
+        bio: user.bio,
+        password: user.password,
+        idRole: user.idRole // Assurez-vous que idRole est fourni
+    });
+}
 
 const logIn = async (email) => {
   return await knex('users').where('email', email).first();
