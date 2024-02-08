fc1a302dafb04da0655a488ee7687bd9be1e9bf9
Wefour <we46584@gmail.com>
2024-02-08 19:33:44 +0100

    profile.js

--- src/Models/Profile.js
+++ src/Models/Profile.js
@@ -3,11 +3,36 @@ const knex = require("../Config/Knex");
 async function getProfileDetails(id){
     return knex 
     .select(
+        "users.idUser",
+        "firstName",
+        "lastName",
         "usersName",
+        "email",
         "bio",
-        "users.idUser"
+        "users.idRole",
+        "role.name",
+        "role.description",
+        "advertisement.idAdvertisement",
+        "title",
+        "advertisement.description",
+        "start_date",
+        "end_date",
+        "created_at",
+        "city",
+        "advertisement.idPlant",
+        "advertisement.idUser",
+        "plant.idPlant",
+        "plant.name as plantName",
+        "plant.description as plantDesc",
+        "image.idAdvertisement",
+        "url",
+        "idImage"
     )
     .from("users")
+    .join("advertisement" , "advertisement.idUser" , "=" , "users.idUser" )
+    .join("role" , "users.idRole" , "=" , "role.idRole")
+    .join("plant" , "advertisement.idPlant" , "=" , "plant.idPlant")
+    .join("image" , " advertisement.idAdvertisement" , "=" , "image.idAdvertisement")
     .where("users.idUser" , id)
 };
 
